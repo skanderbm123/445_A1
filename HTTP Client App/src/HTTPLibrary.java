0dffd9aec2e4683c66896eb9df51cbad84c111db
Eric Kokmanian <erickokmanian1997@gmail.com>
2020-02-02 16:57:01 -0500

    Added empty GET and POST methods

--- HTTP Client App/src/HTTPLibrary.java
+++ HTTP Client App/src/HTTPLibrary.java
@@ -1,9 +1,22 @@
+import java.net.Socket;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class HTTPLibrary {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	// Method representing the GET operation
+	public void GetRequest() {
+		
+	}
+	
+	// Method representing the POST operation
+	public void PostRequest() {
+		
 	}
 
 }
