80c3488716691b978c0e6c2266ee51cda7de7245
Eric Kokmanian <erickokmanian1997@gmail.com>
2020-02-03 19:16:23 -0500

    Added cURL 'help' command for usage

--- HTTP Client App/src/HTTPLibrary.java
+++ HTTP Client App/src/HTTPLibrary.java
@@ -17,8 +17,35 @@ public class HTTPLibrary {
 			else if (args[1].equals("post")){
 				HTTPLibrary.PostRequest(args);
 			}
-			else
-				System.out.println("oops");
+			else {
+				if(args.length == 3 && args[2].equals("post")) {
+					System.out.println("Usage:\n"
+							+ "\thttpc post [-v] [-h key:value] [-d inline-data] [-f file] URL\n"
+							+ "Post executes a HTTP POST request for a given URL with inline data or from file.\n"
+							+ "\t-v: Prints the detail of the response such as protocol, status, and headers.\n"
+							+ "\t-h: key:value Associates headers to HTTP Request with the format 'key:value'.\n"
+							+ "\t-d: string Associates an inline data to the body HTTP POST request.\n"
+							+ "\t-f: file Associates the content of a file to the body HTTP POST request.\n"
+							+ "Either [-d] or [-f] can be used but not both.");
+				}
+				else if(args.length == 3 && args[2].equals("get")) {
+					System.out.println("Usage:\n"
+							+ "\thttpc get [-v] [-h key:value] URL\n"
+							+ "Get executes a HTTP GET request for a given URL.\n"
+							+ "\t-v: Prints the detail of the response such as protocol, status, and headers.\n"
+							+ "\t-h: key:value Associates headers to HTTP Request with the format 'key:value'.");
+				}
+				else {
+					System.out.println("httpc is a curl-like application but supports HTTP protocol only.\n"
+							+ "Usage: \n"
+							+ "\thttpc command [arguments]\n"
+							+ "The commands are:\n"
+							+ "\t- get: executes a HTTP GET request and prints the response.\n"
+							+ "\t- post: executes a HTTP POST request and prints the response.\n"
+							+ "\t- help: prints this screen.\n"
+							+ "Use \"httpc help [command]\" for more information about a command");
+				}
+			}
 		}
 		else {
 			System.out.println("Please enter valid arguments of the form:\n" + 
