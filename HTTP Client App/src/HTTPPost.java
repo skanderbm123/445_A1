bdf1443baf2514f1d81088e1e9e13f47ae1041a0
Skander <skander96@hotmail.com>
2020-02-06 10:59:59 -0500

    added support for headers

--- HTTP Client App/src/HTTPPost.java
+++ HTTP Client App/src/HTTPPost.java
@@ -21,11 +21,13 @@ public class HTTPPost {
 	}
 	
 	public void operation(String[] args) {
+		
 		if(!url.equals("")) {
 			try {				
 				// To get IP address of URL
 				InetAddress ip = InetAddress.getByName(new URL(url).getHost());
 				var socket = new Socket(ip, 80);
+				
 				
 				// Setting up input and output streams
 				InputStream inputStream = socket.getInputStream();
@@ -33,10 +35,20 @@ public class HTTPPost {
 				
 				String body = "key1=value1&key2=value2";
 				
+				String[] headers = CheckHeaders(args);
+				
+				String contextType = "";
+				
+				for(int i=0;i<headers.length;i++) {
+					
+					contextType = headers[i] + "\r\n";
+					
+				}
+				
+				
 				String request = "POST /post HTTP/1.0\r\n"
-						+ "Content-Type: application/x-www-form-urlencoded\r\n"
-						+ "Content-Length: " + body.length() + "\r\n"
-						+ "\r\n"
+						+ "Content-Length: " + body.length() + "\r\n"+
+						contextType+ "\r\n"
 						+ body;
 				
 				outputStream.write(request.getBytes());
@@ -51,6 +63,9 @@ public class HTTPPost {
 					response.append((char) responseData);
 					responseData = inputStream.read();
 				}
+				
+				
+			
 				
 				// Check whether 'verbose' option was passed in command arguments
 				CheckVerbose(args , response);
@@ -67,6 +82,47 @@ public class HTTPPost {
 			System.out.println("Please enter valid arguments of the form:\n" + 
 					"httpc (get|post) [-v] (-h \"k:v\")* [-d inline-data] [-f file] URL");
 		}
+	}
+
+	private String[] CheckHeaders(String[] args) {
+		
+		
+		int index = 0;
+		int count=0;
+		//find index of headers
+		for(int i=0 ; i < args.length ; i++) {
+			
+			if(args[i].equals("-h")) {
+				count++;		
+				if(count==1)
+					index=i;
+			}
+			
+		
+			
+		}
+		
+
+			String[] heads = new String[count];
+			
+			// for headers
+		for(int i = 0 ; i < args.length ; i++) {
+			
+			
+			heads[i] = args[index+1];
+
+		
+			if(args[index+2].equals("-d") || args[index+2].equals("-f") || args[index+2].contains("http"))
+				break;
+
+			index=index+2;
+			
+		}
+		
+		
+		return heads; 
+		
+		
 	}
 
 	private void CheckVerbose(String[] args , StringBuilder response) {
