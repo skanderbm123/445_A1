f5770f6aec65f32ae7fe6ae2e39d46983b45246c
Skander <skander96@hotmail.com>
2020-02-08 22:24:26 -0500

    Fix host field for GET

--- HTTP Client App/src/HTTPGet.java
+++ HTTP Client App/src/HTTPGet.java
@@ -30,6 +30,7 @@ public class HTTPGet {
 		if(!url.equals("")) {
 			try {				
 				// To get IP address of URL
+			
 				InetAddress ip = InetAddress.getByName(new URL(url).getHost());
 				
 				// Checks if given URL is good or not
@@ -37,15 +38,22 @@ public class HTTPGet {
 					System.out.println("URL provided is not a valid one!");
 				
 				Socket socket = new Socket(ip,80);
-				
+			
+			
 				// Setting up input and output streams
 				InputStream inputStream = socket.getInputStream();
 				OutputStream outputStream = socket.getOutputStream();
 				
-				
 				int KeysValue = GetArgs(args);
+				String body;
+				
+				if(KeysValue==-1) {
+					body="";
+				}else {
+					body=args[args.length-1].substring(KeysValue+1);
+				}
 	
-				String request = "GET /get?"+args[args.length-1].substring(KeysValue+1)+" HTTP/1.0\r\n\r\n";
+				String request = "GET /get?"+body+" HTTP/1.1\r\nhost:"+ip.getHostName()+"\r\n\r\n";
 				
 				outputStream.write(request.getBytes());
 				outputStream.flush();
@@ -109,7 +117,7 @@ public class HTTPGet {
 			pw = new PrintWriter(new FileOutputStream(outputTxtFile));
 		}
 		catch(FileNotFoundException e) {
-			System.out.println("File " + outputTxtFile + " not foud.");
+			System.out.println("File " + outputTxtFile + " not found.");
 		}
 		catch(IOException e) {
 			System.out.println("File not created");
